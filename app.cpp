68ca1dcace52858efa4153ecdfcfe4e2f062c222
Thehacker <naelalejandrorengifocontreras@gmail.com>
2026-01-17 11:07:33 -0400

    Update app.cpp

--- app.cpp
+++ app.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
+#include <string>
 #include <chrono>
 #include <thread>
+
+void response_mini_bot() {
+  std::string responses[] = {
+      "Hola, ¿Cómo estás?",
+      "¿Cómo te llamas?",
+      "Que bien",
+      "Entiendo que estes atravesando una situación no tan positiva"
+      };
+}
 
 int main() {
   std::cout << "               ---- Mini Bot ----" << std::endl;
 
+  
   return 0;
 }
