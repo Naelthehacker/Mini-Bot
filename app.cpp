fd0edde060a64553f7eadc8d3465c736e7e4e313
Thehacker <naelalejandrorengifocontreras@gmail.com>
2026-01-17 11:23:09 -0400

    Update app.cpp

--- app.cpp
+++ app.cpp
@@ -3,18 +3,28 @@
 #include <chrono>
 #include <thread>
 
-void response_mini_bot() {
-  std::string responses[] = {
+void response_mini_bot(std::string user_responses) {
+  const std::string responses[] = {
       "Hola, ¿Cómo estás?",
       "¿Cómo te llamas?",
       "Que bien",
       "Entiendo que estes atravesando una situación no tan positiva"
       };
+
+  std::cout << "[•] ${user_responses}" << std::endl;
+  std::cout << "\n";
 }
 
 int main() {
+  std::string response_of_the_user;
+  
   std::cout << "               ---- Mini Bot ----" << std::endl;
 
+  std::cout << "[*] Dí algo: ";
+  std::cin >> response_of_the_user;
+  std::cout << "\n";
+  
+  response_mini_bot(response_of_the_user);
   
   return 0;
 }
